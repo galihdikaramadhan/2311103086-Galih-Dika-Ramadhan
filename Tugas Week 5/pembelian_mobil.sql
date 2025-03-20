f7140b07074407175b160ecca89387cef017bd2e
galihdikaramadhan <dikaramadhangalih@gmail.com>
2025-03-20 16:03:57 +0700

    Add files via upload

---
+++ Tugas Week 5/pembelian_mobil.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS pembelian_mobil;
+USE pembelian_mobil;
+
+CREATE TABLE IF NOT EXISTS pembeli (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    nomor VARCHAR(20) NOT NULL,
+    mobil VARCHAR(50) NOT NULL,
+    alamat TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
